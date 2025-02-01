8e8cc099ca18cae59ed1d8de6ce5eaa95972da4a
jenilparmar <jerryhash231@gmail.com>
2025-02-01 13:03:42 +0530

    Improved

--- src/app/Components/Home.jsx
+++ src/app/Components/Home.jsx
@@ -8,8 +8,8 @@ const pixelFont = Pixelify_Sans({
 
 const Home = () => {
   return (
-    <div className={`${pixelFont.className} self-center `}>
-      <p className="custom-font text-9xl text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-green-500">CRUD-LINGO</p>
+    <div className={`${pixelFont.className} self-center h-1/3 `}>
+      <p className="custom-font text-9xl text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-green-500 animate-gradient">CRUD-LINGO</p>
     </div>
   );
 };
